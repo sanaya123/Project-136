d52e2c94b1179c0e9f10d91d67ab307dc07efc2d
sanaya123 <83179671+sanaya123@users.noreply.github.com>
2022-10-29 12:24:58 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+video="";
+status="";
+
+
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML = "Status :  Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error,results){
+if(error){
+    console.error(error)
+}else{
+    console.log(results);
+    object=results;
+}
+}
